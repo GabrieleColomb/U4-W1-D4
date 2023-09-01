70dad643c1ae8e66cc488cb63dfa7f86414de6e0
GabrieleColombo <133026314+GabrieleColomb@users.noreply.github.com>
2023-09-01 08:58:49 +0200

    Add files via upload

---
+++ U4-W1-D4/U4-W1-D4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("Scegli l’operazione da effettuare:");
+                Console.WriteLine("1.: Login");
+                Console.WriteLine("2.: Logout");
+                Console.WriteLine("3.: Verifica ora e data di login");
+                Console.WriteLine("4.: Lista degli accessi");
+                Console.WriteLine("5.: Esci");
+                Console.WriteLine("========================================");
+                Console.Write("Scelta: ");
+
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Utente.Login();
+                            break;
+                        case 2:
+                            Utente.Logout();
+                            break;
+                        case 3:
+                            Utente.VerificaLogin();
+                            break;
+                        case 4:
+                            Utente.ListaAccessi();
+                            break;
+                        case 5:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Scelta non valida. Riprova.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Scelta non valida. Riprova.");
+                }
+
+            } while (true);
+        }
+    }
+}
---
+++ U4-W1-D4/U4-W1-D4/Utente.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D4
+{
+    internal class Utente
+    {
+    private static string username;
+        private static string password;
+        private static DateTime ultimoLogin;
+        private static DateTime ultimoLogout;
+        private static List<Tuple<string, DateTime, DateTime>> accessi = new List<Tuple<string, DateTime, DateTime>>();
+
+        public static void Login()
+        {
+            Console.Write("Username: ");
+            string inputUsername = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string inputPassword = Console.ReadLine();
+
+            Console.Write("Conferma password: ");
+            string confirmPassword = Console.ReadLine();
+
+            if (inputUsername != "" && inputPassword == confirmPassword)
+            {
+                username = inputUsername;
+                password = inputPassword;
+                ultimoLogin = DateTime.Now;
+                accessi.Add(new Tuple<string, DateTime, DateTime>(username, ultimoLogin, DateTime.MinValue));
+
+                Console.WriteLine("Login effettuato con successo.");
+            }
+            else
+            {
+                Console.WriteLine("Errore nell'autenticazione.");
+            }
+        }
+
+        public static void Logout()
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                ultimoLogout = DateTime.Now;
+                var lastAccess = accessi.FindLast(a => a.Item1 == username);
+                accessi[accessi.IndexOf(lastAccess)] = Tuple.Create(lastAccess.Item1, lastAccess.Item2, ultimoLogout);
+
+                username = null;
+                password = null;
+
+                Console.WriteLine("Logout effettuato.");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato.");
+            }
+        }
+
+        public static void VerificaLogin()
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                Console.WriteLine($"Ultimo login: {ultimoLogin}");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato.");
+            }
+        }
+
+        public static void ListaAccessi()
+        {
+            if (accessi.Count > 0)
+            {
+                Console.WriteLine("Storico accessi:");
+                foreach (var accesso in accessi)
+                {
+                    Console.WriteLine($"Utente: {accesso.Item1}, Login: {accesso.Item2}, Logout: {accesso.Item3}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nessun accesso registrato.");
+            }
+        }
+    }
+}
